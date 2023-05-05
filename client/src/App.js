9b39d8aef3a34cfaa4b3ff11798aac471fb5f965
HodayaChayo <hodayachayo@gmail.com>
2023-05-05 13:44:38 +0300

    start login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header.js';
-import Main from './components/Main/Main.js';
+// import Main from './components/Main/Main.js';
 import Footer from './components/Footer/Footer.js';
+import Login from './components/pages/Login/Login.js';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Main />
+      <Login/>
       <Footer />
     </div>
   );
---
+++ client/src/components/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import Button from '../../Button/Button';
+export default function Loading() {
+  return (
+    <main>
+      <form>
+        <input type='email' name='userName' placeholder='שם משתמש'></input>
+        <input type='email' name='userName' placeholder='שם משתמש'></input>
+        <Button text='כניסה' />
+      </form>
+    </main>
+  );
+}
