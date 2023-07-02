0af9e1f7371acf78414462c45e86d3aa76abf3be
ezracChayu <ezrachayu94@gmail.com>
2023-07-02 23:50:55 +0300

    css  הגדרות משתמש

--- client/src/components/pages/UserSettings/UserSettings.js
+++ client/src/components/pages/UserSettings/UserSettings.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Sidebars from '../../Sidebars/Sidebars';
 import css from './UserSettings.module.css';
 import Button from '../../Button/Button';
+import Header from '../../Header/Header'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
@@ -105,8 +106,9 @@ export default function UserSettings(props) {
     <div className='body'>
       <ToastContainer />
       <Sidebars />
-      <h1>הגדרות עוסק</h1>
-      <h2>פרטי לקוח</h2>
+      <Header title='הגדרות עוסק' />
+
+      <h2>פרטי לקוח:</h2>
       <div className={css.userDetails}>
         <p className={css.p}>
           שם משתמש:
@@ -118,7 +120,7 @@ export default function UserSettings(props) {
             onChange={e => setUserName(e.target.value)}
           ></input>
         </p>
-        <p>
+        <p className={css.p}>
           סיסמה:
           <input
             value={password}
@@ -129,7 +131,7 @@ export default function UserSettings(props) {
             onChange={e => setPassword(e.target.value)}
           ></input>
         </p>
-        <p>
+        <p className={css.p}>
           שם לקוח/חברה:
           <input
             value={cusName}
@@ -140,7 +142,7 @@ export default function UserSettings(props) {
             onChange={e => setCusName(e.target.value)}
           ></input>
         </p>
-        <p>
+        <p className={css.p}>
           טלפון:
           <input
             value={phone}
@@ -151,7 +153,7 @@ export default function UserSettings(props) {
             onChange={e => setPhone(e.target.value)}
           ></input>
         </p>
-        <p>
+        <p className={css.p}>
           ח.פ:
           <input
             value={idVAT}
@@ -164,9 +166,9 @@ export default function UserSettings(props) {
         </p>
       </div>
 
-      <h2>נתוני רשויות</h2>
-      <div className='AuthorityData'>
-        <p>
+      <h2>נתוני רשויות:</h2>
+      <div className={css.AuthorityData}>
+        <p className={css.p1}>
           סוג עוסק:
           <select
             value={type}
@@ -178,7 +180,7 @@ export default function UserSettings(props) {
             <option label='עוסק פטור' value='פטור'></option>
           </select>
         </p>
-        <p>
+        <p className={css.p1}>
           תדירות מע"מ:
           <select
             value={VATFrequency}
@@ -190,7 +192,7 @@ export default function UserSettings(props) {
             <option label='דו חודשי' value='2'></option>
           </select>
         </p>
-        <p>
+        <p className={css.p1}>
           תדירות מ.ה:
           <select
             value={taxFrequency}
@@ -201,7 +203,7 @@ export default function UserSettings(props) {
             <option label='דו חודשי' value='2'></option>
           </select>
         </p>
-        <p>
+        <p className={css.p1}>
           *אחוז מקדמות:
           <input
             value={taxPercent}
@@ -211,13 +213,13 @@ export default function UserSettings(props) {
             onChange={e => setTaxPercent(e.target.value)}
           ></input>
         </p>
-        <p>
+        <p className={css.p1}>
           עובד מטפל:
           <select name='manager' onChange={e => setManager(e.target.value)}>
             <option label='עובד מטפל - לא פעיל'></option>
           </select>
         </p>
-        <p>
+        <p className={css.p1}>
           הערות ללקוח:
           <textarea
             value={note}
