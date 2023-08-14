30e39f2e116ecb64139917ff00c39916d9efebf9
HodayaChayo <hodayachayo@gmail.com>
2023-08-14 23:57:30 +0300

    pdf good

--- client/src/components/pages/ReceivingDocuments/ReceivingDocuments.js
+++ client/src/components/pages/ReceivingDocuments/ReceivingDocuments.js
@@ -1,34 +1,14 @@
 import React, { useState } from 'react';
-// import SingleDocument from '../../DisplayDocuments/SingleDocument';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import DisplayDocuments from '../../DisplayDocuments/DisplayDocuments';
+import PdfViewerComponent from '../../DisplayDocuments/PdfViewerComponent';
 
 import myFile from './cv pdf.pdf';
-import myPhoto from './photoo.jpg'
+import myPhoto from './mahat_tests_search (1).pdf';
 import Sidebar from '../../Sidebars/Sidebars';
 import Footer from '../../Footer/Footer';
 import Header from '../../Header/Header';
 
-
 export default function ReceivingDocuments(props) {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
-  function changePage(offset) {
-    setPageNumber(prevPageNumber => prevPageNumber + offset);
-  }
-
-  function previousPage() {
-    changePage(-1);
-  }
-
-  function nextPage() {
-    changePage(1);
-  }
 
   return (
     <div className='body'>
@@ -59,14 +39,9 @@ export default function ReceivingDocuments(props) {
             // onChange={e => setPassword(e.target.value)}
           ></input>
         </div>
-        {/* <div>
-          <SingleDocument file={myFile}/>
-        </div> */}
-
         <div>
-          <Document file={myFile} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-          </Document>
+          {/* <DisplayDocuments/> */}
+          <PdfViewerComponent document={myFile}/>
         </div>
       </main>
       <Footer />
