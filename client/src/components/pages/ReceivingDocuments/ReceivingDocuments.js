ff596bf52bee6e40971575bcfeb3517e7354eaff
ezracChayu <ezrachayu94@gmail.com>
2023-08-16 21:41:01 +0300

    עזרא

--- client/src/components/pages/ReceivingDocuments/ReceivingDocuments.js
+++ client/src/components/pages/ReceivingDocuments/ReceivingDocuments.js
@@ -2,21 +2,26 @@ import React, { useState } from 'react';
 import DisplayDocuments from '../../DisplayDocuments/DisplayDocuments';
 import PdfViewerComponent from '../../DisplayDocuments/PdfViewerComponent';
 
+import css from './receivingDocuments.module.css';
 import myFile from './cv pdf.pdf';
 import myPhoto from './mahat_tests_search (1).pdf';
 import Sidebar from '../../Sidebars/Sidebars';
 import Footer from '../../Footer/Footer';
 import Header from '../../Header/Header';
 
-export default function ReceivingDocuments(props) {
 
+// const [documentType, setDocumentType] = useState('');
+// const [doubt, setDoubt] = useState('');
+
+export default function ReceivingDocuments(props) {
   return (
     <div className='body'>
       <Sidebar />
       <Header title='קליטת מסמכים' />
-      <main>
-        <div>
-          <select placeholder='סוג מסמך' name='documentType' id=''></select>
+      <main className={css.allMain}>
+        <div className={css.allInput}>
+          <select name='doubt' id=''></select>
+          <select name='TypeExpenditure' id=''></select>
           <input
             type='text'
             name='InvoiceNumber'
@@ -38,10 +43,19 @@ export default function ReceivingDocuments(props) {
             maxLength={20}
             // onChange={e => setPassword(e.target.value)}
           ></input>
+          <input
+            type='text'
+            name='otherVAT'
+            placeholder='מע"מ מאולץ'
+            maxLength={20}
+            // onChange={e => setPassword(e.target.value)}
+          ></input>
+          <textarea name='note' id='' cols='45' rows='10'></textarea>
+          <button>קלוט</button>
         </div>
-        <div>
+        <div className={css.pdf}>
           {/* <DisplayDocuments/> */}
-          <PdfViewerComponent document={myFile}/>
+          <PdfViewerComponent document={myFile} />
         </div>
       </main>
       <Footer />
